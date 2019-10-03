65c0f0c96a2a17e8702d2a1cdb0583f9a3b8c180
Sally Haefling <sallyhaefling@gmail.com>
2019-10-03 11:07:24 -0600

    Initial setup and add header component with styles

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+    <div className='header-container'>
+        <h1 className='header-title'>Furtastic Fotos</h1>
+        <p className='header-subtitle'>Browse through random images of furry friends all in one place!</p>
+    </div>
+    )
+}
+
+export default Header;
