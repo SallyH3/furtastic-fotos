93d237ba95ae08ea4baa46822092d810a07a89fe
Sally Haefling <sallyhaefling@gmail.com>
2019-10-03 15:58:52 -0600

    Add methods for api fetches and create Breeds component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Header from '../Header/Header';
+import Breeds from '../Breeds/Breeds';
 
-function App() {
-  return (
-    <div>
-      <Header />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      photos: [],
+      breedName: '',
+      breeds: [],
+      hasErrors : false
+    }
+  }
+
+  getSelectedBreed = (breedName) => {
+    this.setState({
+      breedName: breedName,
+    })
+    this.selectBreed();
+  }
+
+  selectBreed = (breedName) => {
+    fetch(`https://dog.ceo/api/breed/${breedName}/images`)
+    .then(res => res.json())
+    .then(breed => this.setState({ photos: breed.message }))
+    .catch(() => this.setState({ hasErrors: true }))
+  }
+
+  componentDidMount() {
+    const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+    fetch(breedUrl)
+      .then(res => res.json())
+      .then(data => this.setState({breeds: data.message}))
+      .catch(() => this.setState({ hasErrors: true }))
+  }
+
+  render() {
+    console.log('breed', this.breedName)
+    return (
+      <div>
+        <Header />
+        <Breeds 
+          breeds={this.state.breeds} 
+          getSelectedBreed={this.getSelectedBreed}
+        />
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
     <div className='header-container'>
         <h1 className='header-title'>Furtastic Fotos</h1>
-        <p className='header-subtitle'>Browse through random images of furry friends all in one place!</p>
+        <p className='header-subtitle'>Select a breed to view dogs specific to that breed!</p>
     </div>
     )
 }
